039b1379f5ffdb5e9f1df626bb39cf4565aed776
Nitesh Rathod <nitesh.rathod@orchids.edu.in>
2021-04-30 04:23:10 +0530

    Updates

---
+++ precache-manifest.b51b9dda0579d28bf6302b97b6c23061.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f523470af8d0d376d436aae191fc56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc0eadcc526e6f54bb4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c09b17b3d8498e5981df",
+    "url": "/static/css/main.65f91a77.chunk.css"
+  },
+  {
+    "revision": "8bc0eadcc526e6f54bb4",
+    "url": "/static/js/2.5df3ace6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.5df3ace6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c09b17b3d8498e5981df",
+    "url": "/static/js/main.f91a2c8f.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "7c9bd396246b2990937dc1496a3d9583",
+    "url": "/static/media/Nitesh_Software_Dev.7c9bd396.pdf"
+  }
+]);
